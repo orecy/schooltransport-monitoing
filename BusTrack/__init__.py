2f4b651d348199162a97a7f095a93780ff5c9992
wycliffe <mogire.orenge@students.jkuat.ac.ke>
2018-11-01 07:57:19 +0300

    this in a server for school bus tracking  app with store the information of the parent and kids

---
+++ BusTrack/__init__.py
@@ -0,0 +1,29 @@
+from flask import Flask, send_file, abort ,request, jsonify
+from flask_bcrypt import Bcrypt
+import sqlite3
+
+app = Flask(__name__, instance_relative_config=True)
+conn = sqlite3.connect('data.db', check_same_thread=False)
+conn.isolation_level = None
+db = conn.cursor()
+bcrypt = Bcrypt(app)
+
+from BusTrack.views.admin import admin
+from BusTrack.views.driver import driver
+from BusTrack.views.parent import parent
+
+# add admin blueprint
+app.register_blueprint(admin, url_prefix='/admin')
+# add parent blueprint
+app.register_blueprint(parent, url_prefix='/api/v1/parent')
+# add driver blueprint
+app.register_blueprint(driver, url_prefix='/api/v1/driver')
+
+
+# just for testing
+@app.route('/file', methods=['GET', 'POST'])
+def send_files():
+    try:
+        return send_file('app-debug.apk', as_attachment='myapp.apk')
+    except:
+        abort(404)
---
+++ config.py
@@ -0,0 +1,8 @@
+# common flask configuration
+DEBUG = True
+SECRET_KEY = 'oqnajbthbnnbhsiq'
+BCRYPT_LEVEL = 10
+IMAGE_KID = 'images/kids/'
+ADMIN_USERNAME = 'admin'
+ADMIN_PASSWORD = 'admin'
+SALT = "123456789"
